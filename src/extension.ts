6e789ba3988dc3427624ab43f511f5e2fe1b5b60
phobal <phobal@126.com>
2020-08-09 15:17:08 +0800

    feat: add logic code

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const nodeModulesPaths = '**/node_modules'
+
+let config = vscode.workspace.getConfiguration()
+let excluded = config.get('files.exclude', {} as Record<string, boolean>)
+
+function removeNodeModulesFolder() {
+	excluded[nodeModulesPaths] = true
+	config.update('files.exclude', excluded)
+}
+
+function addNodeModulesFolder() {
+	excluded[nodeModulesPaths] = false
+	config.update('files.exclude', excluded)
+}
+
+async function collapse() {
+	await removeNodeModulesFolder()
+	await addNodeModulesFolder()
+}
+
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "collapse-node-modules" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('collapse-node-modules.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from collapse-node_modules!');
+	let disposable = vscode.commands.registerCommand('collapse-node-modules.collapse', () => {
+		collapse()
 	});
 
 	context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
