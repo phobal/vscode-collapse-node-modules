175efbfc2cbb50e2a76868be93309bf5655b6ac5
冯博 <fengb@chinawyny.com>
2022-06-02 16:30:54 +0800

    fix: code format

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,44 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-const nodeModulesPaths = '**/node_modules'
+const nodeModulesPaths = "**/node_modules";
 
-let config = vscode.workspace.getConfiguration()
-let excluded: Record<string, boolean> | undefined
+let config = vscode.workspace.getConfiguration();
+let excluded: Record<string, boolean> | undefined;
 
 async function removeNodeModulesFolder() {
-	await config.update('files.exclude', {
-		...excluded,
-		[nodeModulesPaths]: true
-	}, vscode.ConfigurationTarget.Global)
+  await config.update(
+    "files.exclude",
+    {
+      ...excluded,
+      [nodeModulesPaths]: true,
+    },
+    vscode.ConfigurationTarget.Global
+  );
 }
 
 async function addNodeModulesFolder() {
-	await config.update('files.exclude', excluded, vscode.ConfigurationTarget.Global)
+  await config.update(
+    "files.exclude",
+    excluded,
+    vscode.ConfigurationTarget.Global
+  );
 }
 
 async function collapse() {
-	excluded = config.inspect<Record<string, boolean> | undefined>('files.exclude')?.globalValue;
-	await removeNodeModulesFolder()
-	await addNodeModulesFolder()
+  excluded = config.inspect<Record<string, boolean> | undefined>(
+    "files.exclude"
+  )?.globalValue;
+  await removeNodeModulesFolder();
+  await addNodeModulesFolder();
 }
 
-
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('collapse-node-modules.collapse', () => {
-		collapse()
-	});
+  let disposable = vscode.commands.registerCommand(
+    "collapse-node-modules.collapse",
+    () => {
+      collapse();
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
